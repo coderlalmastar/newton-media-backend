9e05dc0208b654c9f413ae6ccb447347fbc9991f
coderlalmastar <139443574+coderlalmastar@users.noreply.github.com>
2023-07-16 13:19:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,19 +29,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-const allowedOrigin = 'https://harmonious-faloodeh-3b7b17.netlify.app/';
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigin.indexOf(origin) !== -1) {
-        callback(null, true)
-    } else {
-        callback(new Error('Not allowed by CORS'))
-    }
-},
-optionsSuccessStatus:200,
-methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-allowedHeaders: ['X-PINGOTHER', 'Content-Type',"Authorization","Origin", 'HEAD', 'OPTIONS',"Accept","Cache-Control",'Cookie','X-Requested-With'],
-credentials: true
+  origin: 'https://harmonious-faloodeh-3b7b17.netlify.app',
+  optionsSuccessStatus:200,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-PINGOTHER', 'Content-Type',"Authorization","Origin", 'HEAD', 'OPTIONS',"Accept","Cache-Control",'Cookie','X-Requested-With'],
+  credentials: true
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
