833d0b5660a6cd8d5c0fdd6fdd67fe2be8834a0c
coderlalmastar <139443574+coderlalmastar@users.noreply.github.com>
2023-07-18 22:08:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(cors({
         } else {
             callback(new Error('Not allowed by CORS'))
         }
-    },,
+    },
   optionsSuccessStatus:200,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['X-PINGOTHER', 'Content-Type',"Authorization","Origin", 'HEAD', 'OPTIONS',"Accept","Cache-Control",'Cookie','X-Requested-With'],
