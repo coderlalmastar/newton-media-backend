7effac1399c49e1af7e06260d464f6dd6ca302db
coderlalmastar <139443574+coderlalmastar@users.noreply.github.com>
2023-07-18 22:02:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: 'https://harmonious-faloodeh-3b7b17.netlify.app',
+  // origin: 'https://harmonious-faloodeh-3b7b17.netlify.app',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus:200,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['X-PINGOTHER', 'Content-Type',"Authorization","Origin", 'HEAD', 'OPTIONS',"Accept","Cache-Control",'Cookie','X-Requested-With'],
